b15f530079b91b68bd227898e343a8e0d20a9933
Linus Sunde <linus.sunde@gmail.com>
2016-05-21 16:22:32 +0200

    Remove submodule.

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -15,7 +15,7 @@ module.exports = {
     ]
   },
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, '../stellaris-dist'),
     filename: '[name]' + outputFileTemplateSuffix + '.js',
     chunkFilename: '[id]' + outputFileTemplateSuffix + '.js'
   },
