a0abc73cf7fc1b18a40f2d717982f11712621e53
Linus Sunde <linus.sunde@gmail.com>
2016-05-27 23:38:57 +0200

    Handle greater and less than.

--- src/logic/grammar.js
+++ src/logic/grammar.js
@@ -1,20 +1,18 @@
 /* http://pegjs.org/online
 
-
-
 {
 // [ { key: "key1.key2", value: "value" } ]
 // -> { key1: { key2: value } }
-function assignments(kvps) {
+function assignments(asses) {
   let result = {};
-  for (let kvp of kvps) {
-    let keys = kvp.key.split(".");
+  for (let ass of asses) {
+    let keys = ass.key.split(".");
     let current = result;
     for (let i = 0; i < keys.length - 1; i++) {
       current[keys[i]] = {};
       current = current[keys[i]];
     }
-    current[keys[keys.length - 1]] = kvp.value;
+    current[keys[keys.length - 1]] = { oper: ass.oper, value: ass.value } ;
   }
   return result;
 }
@@ -22,9 +20,9 @@ function assignments(kvps) {
 
 script = caw v:assignment* caw { return assignments(v) }
 
-assignment = k:var caw equals caw v:(assignmentList / assignedValue) caw { return { key: k, value: v } }
+assignment = k:var caw o:oper caw v:(assignmentList / assignedValue) caw { return { key: k, oper: o, value: v } }
 
-assignmentList = (open caw v:assignment+ caw close) { return assignments(v) }
+assignmentList = (open caw v:assignment* caw close) { return assignments(v) }
 
 assignedValue = v:(single / list) caw { return v }
 
@@ -36,118 +34,20 @@ varName = $([a-zA-Z_][a-zA-Z0-9_]*)
 
 number = $([-]?([0] / [1-9][0-9]*)([\\.][0-9]+)?)
 
+oper = equals / greater / less
+
 caw "Comments and Whitespace" = (c / mws)*
 c "Comment" = $([#]+[^\n]*)
 ws "Whitespace" = [ \t\n]*
 mws "Mandatory Whitespace" = [ \t\n]+
     
 equals = '='
+greater = '>'
+less = '<'
 open = '{'
 close = '}'
 at = '@'
 quotes = '"'
 dot = '.'
-
-
-
-
-
-
-
-
-
-
-script = ws? s:(comment / macroAssignment / assignment / s:anything)*
-	{ return { rule: "script" , value: s } }
-comment = c:$([#]+[^\n]*) ws?
-	{ return { rule: "comment", value: c } }
-macroAssignment = i:macroIdentifier ws? equals ws? n:number ws?
-	{ return { rule: "macroAssignment(" + i.rule + ", " + n.rule + ")", value: { macroIdentifier: i.value, value: n.value } } }
-assignment = i:identifier ws? equals ws? v:assignedValue ws?
-	{ return { rule: "assignment", value: { identifier: i.value, value: v } } }
-
-assignedValue = (open ws? v:assignment+ ws? close) { return { rules: "assignedValue", value: v } }
-    / v:identifier { return { rules: "assignedValue", value: v } }
-
-
-macroIdentifier = i:$(at identifier) ws?
-	{ return { rule: "macroIdentifier", value: i } }
-//macroList = open macroIdentifier+ close ws?
-stringIdentifier = i:$(quotes identifier quotes) ws?
-	{ return { rule: "stringIdentifier", value: i } }
-//stringList = open stringList+ close ws?
-identifier = i:$([a-zA-Z0-9_]+)
-	{ return { rule: "identifier", value: i } }
-
-number = n:$(([0] / [1-9][0-9]*)([\\.][0-9]+)?)
-	{ return { rule: "number(" + n.rule + ")", value: n } }
-
-ws = [ \t\n]+
-	{ return { rule: "ws", value: undefined } }
-    
-equals = '='
-open = '{'
-close = '}'
-at = '@'
-quotes = '"'
-    
-anything = a:$.+
-	{ return { rule: "anything", value: a } }
-
-
-
-
-
-
-
-script -> assignment*
-assignment -> identifier operator value | identifier = { assignment* }
-operator -> = | >
-variable -> string | number | identifier
-string -> ".*"
-number -> [0-9](.[0-9]+)?
-identifier -> .* (Och är inte string eller number)
-
-
-
-
-
-script = s:(macro / ws / s:anything)*
-	{ return { rule: "s" , value: s } }
-macro = at i:identifier ws equals ws n:number ws
-	{ return { rule: "m(" + i.rule + ", " + n.rule + ")", value: { macro: i.value, value: n.value } } }
-at = '@'
-identifier = i:$([a-zA-Z0-9]+)
-	{ return { rule: "i", value: i } }
-anything = .+
-equals = '='
-number = n:(n1 / n2 / n3 / n4)
-	{ return { rule: "n(" + n.rule + ")", value: n.value } }
-n1 = n:$([1-9][0-9]*[\\.][0-9]+)
-	{ return { rule: "n1", value: n } }
-n2 = n:$([0][\\.][0-9]+)
-	{ return { rule: "n2", value: n } }
-n3 = n:$([1-9][0-9]*)
-	{ return { rule: "n3", value: n } }
-n4 = n:$([0])
-	{ return { rule: "n4", value: n } }
-ws = [ \t\n]+
-	{ return { rule: "ws", value: null } }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 */
